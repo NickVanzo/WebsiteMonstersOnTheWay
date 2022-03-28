065a7ae136211bba9accb6318409ecbeb3433722
Nick <nickvanzo437@gmail.com>
2022-03-28 13:36:54 +0200

    Changed smart contracts

--- website/src/components/marketplace/nftcard.js
+++ website/src/components/marketplace/nftcard.js
@@ -6,6 +6,8 @@ import Typography from '@mui/material/Typography';
 import { ethers } from 'ethers';
 import { constants } from '../../constants';
 import { CircularProgress } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import axios from 'axios';
 
 export default class NFTCard extends React.Component {
     constructor(props) {
@@ -14,7 +16,9 @@ export default class NFTCard extends React.Component {
             imgToDisplay: 1,
             srcOfImgToDisplay: '/images/1.jpeg',
             feeETH: 0,
-            processingMint: false,
+            processingIndexing: false,
+            processingEnded: false,
+            cardMinted: {}
         }
     }
 
@@ -31,7 +35,11 @@ export default class NFTCard extends React.Component {
         let provider = new ethers.providers.Web3Provider(window.ethereum);
         let signer = provider.getSigner();
         let contract = new ethers.Contract(constants.contractNFTAddress, constants.contractNFTABI, signer);
-        return contract;
+        return {
+            contract: contract,
+            provider: provider,
+            address: signer.getAddress()
+        };
     }
 
     loadNextImage = (index) => {
@@ -45,29 +53,97 @@ export default class NFTCard extends React.Component {
         let i = 1;
         const numberOfCards = 7;
         setInterval(() => {
-            this.loadNextImage((this.state.imgToDisplay % numberOfCards) + 1);
-            i++;
+            if (!this.state.processingEnded) {
+                this.loadNextImage((this.state.imgToDisplay % numberOfCards) + 1);
+                i++;
+            }
         }, 500);
     }
 
-    handleMintWithGold = async () => {
-        let contract = this.connectToSmartContract();
+    generateRandomURI = () => {
+        let id = Math.floor(Math.random() * 6);
+        const uri = this.randomUri(id);
+        return {
+            uri: uri,
+            id: id
+        };
+    }
+
+    mintCard = () => {
 
     }
 
-    handleMintWithETH = async () => {
-        let contract = this.connectToSmartContract();
-        const fee = await this.fetchFee(contract);
-        let id = Math.floor(Math.random() * 6);
-        const uri = this.randomUri(id);
-        // const trx = await contract.safeMint(`${fee}`, `${uri}`);
+    handleMintWithPromethium = async () => {
+        let blockchain = this.connectToSmartContract();
+        let uri = this.generateRandomURI();
+        let card = {
+            id: uri.id,
+            name: this.nameOfCard(uri.id),
+            uri: uri.uri
+        }
         this.setState({
-            processingMint: true
+            cardMinted: card
+        })
+        let trx = await blockchain.contract.safeMintWithTokens(`${uri.uri}`);
+        this.setState({
+            processingIndexing: true
+        })
+        let receipt = await blockchain.provider.waitForTransaction(trx.hash);
+        card.hash = receipt.transactionHash
+        let img = this.getImgFromName(card.name);
+        this.setState({
+            processingEnded: true,
+            srcOfImgToDisplay: img
         })
     }
 
-    fetchFee = async (contract) => {
-        const feeBN = await contract.getFee();
+    handleMintWithETH = async () => {
+        let blockchain = this.connectToSmartContract();
+        const fee = await this.fetchFee(blockchain);
+        let uri = this.generateRandomURI();
+        const options = {
+            value: ethers.utils.parseEther(`${fee}`)
+        }
+        let card = {
+            id: uri.id,
+            name: this.nameOfCard(uri.id),
+            uri: uri.uri
+        }
+        this.setState({
+            cardMinted: card
+        })
+        const trx = await blockchain.contract.safeMint(`${uri.uri}`, options);
+        this.setState({
+            processingIndexing: true
+        })
+        let receipt = await blockchain.provider.waitForTransaction(trx.hash);
+
+        card.hash = receipt.transactionHash
+        let img = this.getImgFromName(card.name);
+        this.setState({
+            processingEnded: true,
+            srcOfImgToDisplay: img
+        })
+    }
+
+    nameOfCard = (id) => {
+        if (id === 0) {
+            return "Wrath"
+        } else if (id === 1) {
+            return "Midnight Hunt"
+        } else if (id === 2) {
+            return "Blade of Rashes"
+        } else if (id === 3) {
+            return "Vampire"
+        } else if (id === 4) {
+            return "Sharpening"
+        } else if (id === 5) {
+            return "Path of gold"
+        }
+    }
+
+    fetchFee = async (blockchain) => {
+        const feeBN = await blockchain.contract.getFee();
         return ethers.utils.formatEther(feeBN._hex).toString();
     }
 
@@ -75,28 +151,63 @@ export default class NFTCard extends React.Component {
         let uri = '';
         switch (id) {
             case 0:
-                uri = 'ipfs://QmcASoo86H9dboZoL6CQ18MBPbaVkF82ydr7mBUwzXMmYh';
+                uri = 'QmcASoo86H9dboZoL6CQ18MBPbaVkF82ydr7mBUwzXMmYh';
                 break;
             case 1:
-                uri = 'ipfs://QmUVccUPYCnaJqT6NrDjfiScQdjTiBaBrFRDuFXV6snL3t';
+                uri = 'QmUVccUPYCnaJqT6NrDjfiScQdjTiBaBrFRDuFXV6snL3t';
                 break;
             case 2:
-                uri = 'ipfs://QmWcqbvK5qkJRkrfokhnQHk6tc6hzjQjM3ZuRiQLa7KQBv';
+                uri = 'QmWcqbvK5qkJRkrfokhnQHk6tc6hzjQjM3ZuRiQLa7KQBv';
                 break;
             case 3:
-                uri = 'ipfs://QmQLVp88bgYvpgYh9aPowHKSQ3RCaT9s472yoirgwXyDaW';
+                uri = 'QmQLVp88bgYvpgYh9aPowHKSQ3RCaT9s472yoirgwXyDaW';
                 break;
             case 4:
-                uri = 'ipfs://QmWCvjL4JKWuwmbK6rCjGAWnEztnW8DMHNZDAucq6thXmx';
+                uri = 'QmWCvjL4JKWuwmbK6rCjGAWnEztnW8DMHNZDAucq6thXmx';
                 break;
             case 5:
-                uri = 'ipfs://QmP4dt9ByjwDqwCvx2goTUCcFuU9u7DhhyfhnPb6mtGWa2';
+                uri = 'QmP4dt9ByjwDqwCvx2goTUCcFuU9u7DhhyfhnPb6mtGWa2';
                 break;
             default:
                 console.error('There are not uris for this id');
                 break;
         }
         return uri;
+    }
+
+    getImgFromName = () => {
+        let img = "/images/";
+        switch (this.state.cardMinted.name) {
+            case "Wrath":
+                img = "/images/6.jpeg";
+                break;
+            case "Path of gold":
+                img = "/images/5.jpeg";
+                break;
+            case "Vampire":
+                img = "/images/2.jpeg";
+                break;
+            case "Midnight Hunt":
+                img = "/images/3.jpeg";
+                break;
+            case "Blade of Rashes":
+                img = "/images/7.jpeg";
+                break;
+            case "Sharpening":
+                img = "/images/4.jpeg";
+                break;
+            default:
+                console.error("There is not image for this id");
+                break;
+        }
+        return img;
+    }
+
+    handleDoneButton = () => {
+        this.setState({
+            processingIndexing: false,
+            processingEnded: false
+        })
     }
 
     render() {
@@ -111,30 +222,38 @@ export default class NFTCard extends React.Component {
                     />
                     <CardContent>
                         {
-                            !this.state.processingMint ? (
+                            !this.state.processingIndexing ? (
                                 <Typography gutterBottom variant="h5" component="div">
                                     <button className={'personal-button-mint-eth'} onClick={this.handleMintWithETH}>
                                         {this.state.feeETH} ETH
                                     </button>
-                                    <button className={'personal-button-mint-gold'} onClick={this.handleMintWithGold}>
-                                        1000 gold coins
-                                    </button>
-                                    <button className={'personal-button-mint-promethium'}>
+                                    <button className={'personal-button-mint-promethium'} onClick={this.handleMintWithPromethium}>
                                         10 promethiums
                                     </button>
                                 </Typography>
                             ) : (
-                                <CircularProgress />
+                                !this.state.processingEnded ? (
+                                    <>
+                                        <p style={{ color: "white" }}>Processing...
+                                        </p>
+                                        <CircularProgress />
+                                    </>
+                                ) : (
+                                    <>
+                                        <p style={{ color: "white" }}>
+                                            {this.state.cardMinted.name} added to your collection!
+                                        </p>
+                                        <CheckIcon style={{ color: "green" }} />
+                                    <button className='personal-button-accept' onClick={this.handleDoneButton}>Done</button>
+                                    </>
+                                )
                             )
                         }
                         <Typography variant="body2" color="text.secondary">
-
                         </Typography>
                     </CardContent>
                 </Card>
             </>
-
-
         );
     }
 }
